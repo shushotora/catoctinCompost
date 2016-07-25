a4b4bf6859259c60b3a1f576f3410a95ebd39ccf
Shushotora <Shushotora@192.168.1.146>
2016-07-25 19:07:47 -0400

    Added descriptions for products and added new order.

--- app/productsAndServices/productsAndServices.js
+++ app/productsAndServices/productsAndServices.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('catoctinApp.products', [ 'ngRoute' ]).config(
-	[ '$routeProvider', function($routeProvider) {
-	    $routeProvider.when('/products', {
-		templateUrl : 'productsAndServices/productsAndServices.html',
-		controller : 'ProductsCtrl'
-	    });
-	} ]).controller('ProductsCtrl',
-	[ '$scope', '$http', function($scope, $http) {
-	    $http.get('productsAndServices/products.json').success(function(data) {
-		$scope.products = data;
-	    });
-	    $scope.orderProp = 'relevance';
-	} ]);
+    [ '$routeProvider', function($routeProvider) {
+      $routeProvider.when('/products', {
+        templateUrl : 'productsAndServices/productsAndServices.html',
+        controller : 'ProductsCtrl'
+      });
+    } ]).controller('ProductsCtrl',
+    [ '$scope', '$http', function($scope, $http) {
+      $http.get('productsAndServices/products.json').success(function(data) {
+        $scope.products = data;
+      });
+      $scope.orderProp = 'relevance';
+    } ]);
